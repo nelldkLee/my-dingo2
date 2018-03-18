50a2d57eb832697c16a9b29468cbd05d3ae60910
Leedongkyu <nelldklee@gmail.com>
2018-03-18 10:37:12 +0900

    initail commit

---
+++ src/main/java/org/dingo/MyDingoApplication.java
@@ -0,0 +1,12 @@
+package org.dingo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyDingoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyDingoApplication.class, args);
+	}
+}
